502952fe9eb6ff97aed283aa4d59e7217283b905
gusht <gusht@DESKTOP-TC4Q7OA>
2026-02-12 21:42:05 +0200

    Changed the main idea anf the name of the project due to inability to provide user protection - Changed the 'BookExchange' model to 'Movie' model. - Configured the new relationships - Added additional validation constants for the 'Movie' model. - Removed the old migrations due to a problem with the Docker Container

--- BookExchangePlatform/Program.cs
+++ BookExchangePlatform/Program.cs
@@ -17,10 +17,10 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<BookExchangeDbContext>();
 
- 
+    // Ensure database is created
     dbContext.Database.EnsureCreated();
 
-    // Seed
+    // Seed a user if none exists
     if (!dbContext.Users.Any())
     {
         dbContext.Users.Add(new BookExchangePlatform.Models.User
@@ -32,14 +32,18 @@ using (var scope = app.Services.CreateScope())
             Location = "Test City"
         });
         dbContext.SaveChanges();
-        Console.WriteLine("Seed user created with ID: 1");
+        Console.WriteLine("✅ Seed user created with ID: 1");
     }
 }
 
 
+
+
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
