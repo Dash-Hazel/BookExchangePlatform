b0c9e3ac7dc2437b672c6306d2173fdba0e09e71
gusht <gusht@DESKTOP-TC4Q7OA>
2026-03-15 13:11:33 +0200

    Add ReviewsController and WishListsController with full CRUD operations

---
+++ BookExchangePlatform/Controllers/ReviewsController.cs
@@ -0,0 +1,90 @@
+﻿using BookExchangePlatform.Data;
+using BookExchangePlatform.Models;
+using BookExchangePlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookExchangePlatform.Controllers
+{
+    [Authorize]
+    public class ReviewsController : Controller
+    {
+
+        private readonly IReviewService currReviewService;
+        private readonly BookExchangeDbContext currContext;
+        private readonly UserManager<User> userManager;
+
+
+        public ReviewsController(IReviewService reviewtService, BookExchangeDbContext context, UserManager<User> userManager)
+        {
+            currReviewService = reviewtService;
+            currContext = context;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Review review)
+        {
+            
+
+            ModelState.Remove("Owner");
+
+            if (review.CreatedAt == default)
+            {
+                review.CreatedAt = DateTime.Now;
+            }
+
+            if (ModelState.IsValid)
+            {
+                review.OwnerId = userManager.GetUserId(User);
+                await currReviewService.CreateReviewAsync(review);
+                if (review.BookId == null)
+                    return RedirectToAction("Details", "Movies", new { id = review.MovieId });
+
+                else
+                    return RedirectToAction("Details", "Books", new { id = review.BookId });
+            }
+
+
+            return View(review);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var review = await currReviewService.GetReviewByIdAsync(id.Value);
+
+            if (review == null) return NotFound();
+
+            return View(review);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Console.WriteLine($"DeleteConfirmed HIT with id: {id}");
+            var review = await currReviewService.GetReviewByIdAsync(id);
+
+            if (review == null) return NotFound();
+
+            await currReviewService.DeleteReviewAsync(id);
+
+            if (review.BookId == null)
+             return RedirectToAction("Details", "Movies", new { id = review.MovieId });
+
+            else
+             return RedirectToAction("Details", "Books", new { id = review.BookId });
+
+
+        }
+    }
+}
---
+++ BookExchangePlatform/Controllers/WishListsController.cs
@@ -0,0 +1,58 @@
+﻿using BookExchangePlatform.Data;
+using BookExchangePlatform.Models;
+using BookExchangePlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookExchangePlatform.Controllers
+{
+    [Authorize]
+    public class WishListsController : Controller
+    {
+
+        private readonly IWishListService currWishListService;
+        private readonly BookExchangeDbContext currContext;
+        private readonly UserManager<User> userManager;
+
+
+        public WishListsController(IWishListService wishListService, BookExchangeDbContext context, UserManager<User> userManager)
+        {
+            currWishListService = wishListService;
+            currContext = context;
+            this.userManager = userManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = userManager.GetUserId(User);
+            var wishLists = await currWishListService.GetUserWishListAsync(userId);
+            return View(wishLists);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddToWishList(int? bookId, int? movieId)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = userManager.GetUserId(User);
+                await currWishListService.AddItemToWishListAsync(userId, bookId, movieId);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveItemFromWishList(int id)
+        {
+            await currWishListService.RemoveFromWishListAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
